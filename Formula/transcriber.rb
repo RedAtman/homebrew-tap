70b67678186988c3157149a22518b6b412db5e5b
redatman <evicmx@gmail.com>
2026-05-02 01:16:35 +0800

    feat: add transcriber formula

---
+++ Formula/transcriber.rb
@@ -0,0 +1,39 @@
+class Transcriber < Formula
+  desc "Fast video transcription CLI powered by whisper.cpp"
+  homepage "https://github.com/RedAtman/transcriber"
+  license "MIT"
+
+  depends_on "ffmpeg"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.0/transcriber-macos-aarch64"
+      sha256 "__PLACEHOLDER_MACOS_ARM64__"
+
+      def install
+        bin.install "transcriber-macos-aarch64" => "transcriber"
+      end
+    end
+    on_intel do
+      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.0/transcriber-macos-x86_64"
+      sha256 "__PLACEHOLDER_MACOS_X86_64__"
+
+      def install
+        bin.install "transcriber-macos-x86_64" => "transcriber"
+      end
+    end
+  end
+
+  on_linux do
+    url "https://github.com/RedAtman/transcriber/releases/download/v0.1.0/transcriber-linux-x86_64"
+    sha256 "__PLACEHOLDER_LINUX_X86_64__"
+
+    def install
+      bin.install "transcriber-linux-x86_64" => "transcriber"
+    end
+  end
+
+  test do
+    system "#{bin}/transcriber", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# RedAtman Homebrew Tap
+
+```bash
+brew tap RedAtman/tap
+brew install transcriber
+```
+
+Formulas are automatically updated on each release.
