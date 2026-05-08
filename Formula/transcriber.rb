5b80bf1c84b5fcb47cdbaae4f0710143976363c0
RedAtman <evicmx@gmail.com>
2026-05-08 16:49:53 +0000

    chore: update transcriber formula for v0.1.6

--- Formula/transcriber.rb
+++ Formula/transcriber.rb
@@ -7,7 +7,7 @@ class Transcriber < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.5/transcriber-macos-aarch64"
+      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.6/transcriber-macos-aarch64"
       sha256 "ad43bb3aa5adfcf367dabbef04d8035ea434c7329bfb009a7e1baea975bd5035"
 
       def install
@@ -15,7 +15,7 @@ class Transcriber < Formula
       end
     end
     on_intel do
-      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.5/transcriber-macos-x86_64"
+      url "https://github.com/RedAtman/transcriber/releases/download/v0.1.6/transcriber-macos-x86_64"
       sha256 "ad43bb3aa5adfcf367dabbef04d8035ea434c7329bfb009a7e1baea975bd5035"
 
       def install
@@ -25,7 +25,7 @@ class Transcriber < Formula
   end
 
   on_linux do
-    url "https://github.com/RedAtman/transcriber/releases/download/v0.1.5/transcriber-linux-x86_64"
+    url "https://github.com/RedAtman/transcriber/releases/download/v0.1.6/transcriber-linux-x86_64"
     sha256 "0633986d9ee08dcf5e9b7f8fdcc7f43837fed22bea37ef0bc3d37ad63283e487"
 
     def install
